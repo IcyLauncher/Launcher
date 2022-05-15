a8f6dacb1adbb04312cb78d77de41c4242496291
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-05-15 10:03:43 +0300

    Set up hosting & dependency injection (+ log, config)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,16 +1,40 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace IcyLauncher;
 
 public partial class App : Application
 {
+    readonly IHost host;
+    public static IServiceProvider Provider { get; private set; } = default!;
+
     public App()
     {
-        InitializeComponent();
+        host = Host.CreateDefaultBuilder()
+            .UseSerilog((context, configuration) =>
+            {
+                configuration.WriteTo.Debug();
+                configuration.WriteTo.File("Logs\\Log-.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 10);
+            })
+            .ConfigureAppConfiguration((context, builder) =>
+            {
+                builder.AddJsonFile("Configuration.json", true);
+            })
+            .ConfigureServices((context, services) =>
+            {
+
+            })
+            .Build();
+
+        Provider = host.Services;
     }
 
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
     {
+        await host.StartAsync();
 
+        var shell = Provider.GetRequiredService<ShellView>();
+        shell.Activate();
     }
 }
