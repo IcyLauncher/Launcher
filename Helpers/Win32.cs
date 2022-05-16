4909d073a820d54e48131c0870e90d5c701293a9
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-05-16 20:42:02 +0300

    i hate windows 10

--- Helpers/Win32.cs
+++ Helpers/Win32.cs
@@ -8,6 +8,9 @@ namespace IcyLauncher.Helpers;
 
 public class Win32
 {
+    readonly WindowHandler windowHandler = App.Provider.GetRequiredService<WindowHandler>();
+
+
     public delegate IntPtr WinProc(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
 
     public static WinProc? NewWndProc { get; set; } = null;
@@ -26,12 +29,6 @@ public class Win32
     [DllImport("user32.dll")]
     public static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
 
-    [DllImport("User32.dll")]
-    public static extern IntPtr MonitorFromWindow(IntPtr hWnd, int dwFlags);
-
-    [DllImport("User32.dll", CharSet = CharSet.Auto)]
-    internal static extern bool GetMonitorInfo(IntPtr hmonitor, [In, Out] MONITORINFOEX info);
-
 
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
     public struct POINT
@@ -48,26 +45,6 @@ public class Win32
         public POINT ptMaxPosition;
         public POINT ptMinTrackSize;
         public POINT ptMaxTrackSize;
-    }
-
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto, Pack = 4)]
-    public class MONITORINFOEX
-    {
-        public int cbSize = Marshal.SizeOf(typeof(MONITORINFOEX));
-        public RECT rcMonitor = new RECT();
-        public RECT rcWork = new RECT();
-        public int dwFlags = 0;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
-        public char[] szDevice = new char[32];
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct RECT
-    {
-        public int left;
-        public int top;
-        public int right;
-        public int bottom;
     }
 
 
