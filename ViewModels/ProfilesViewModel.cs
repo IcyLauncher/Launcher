4d71de04c8b270b00e971a7d5b81311f8b6f49d8
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-05-26 21:27:34 +0300

    Added profiles page

---
+++ ViewModels/ProfilesViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace IcyLauncher.ViewModels;
+
+public partial class ProfilesViewModel : ObservableObject
+{
+    readonly ILogger logger;
+    readonly public Configuration Configuration;
+    readonly WindowHandler windowHandler;
+
+    public ProfilesViewModel(ILogger<ShellViewModel> logger, IOptions<Configuration> configuration, WindowHandler windowHandler)
+    {
+        this.logger = logger;
+        Configuration = configuration.Value;
+        this.windowHandler = windowHandler;
+    }
+}
---
+++ Views/ProfilesView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using IcyLauncher.ViewModels;
+using Microsoft.UI.Xaml.Controls;
+
+namespace IcyLauncher.Views;
+
+public sealed partial class ProfilesView : Page
+{
+    readonly ProfilesViewModel viewModel = App.Provider.GetRequiredService<ProfilesViewModel>();
+
+    public ProfilesView()
+    {
+        InitializeComponent();
+    }
+}
