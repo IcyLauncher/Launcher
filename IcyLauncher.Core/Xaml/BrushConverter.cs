241d4de7f8e21e2e32f3e1c24f83fe182eca1f76
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-06-05 22:32:29 +0300

    Moved xaml helpers/converters

---
+++ IcyLauncher.Core/Xaml/BrushConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using Windows.UI;
+
+namespace IcyLauncher.Core.Xaml;
+
+public class BrushConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) =>
+        new SolidColorBrush((Color)value);
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+        ((SolidColorBrush)value).Color;
+}
