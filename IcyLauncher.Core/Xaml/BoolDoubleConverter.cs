53f030c5cd334b129cce52462919bb122e7d9f9f
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-07-10 13:57:33 +0300

    Added bool converters

---
+++ IcyLauncher.Core/Xaml/BoolDoubleConverter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace IcyLauncher.Core.Xaml;
+
+public class BoolDoubleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) =>
+        (bool)value ? 1.0 : 0.0;
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+        (double)value == 1.0;
+}
---
+++ IcyLauncher.Core/Xaml/InvertBool.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace IcyLauncher.Core.Xaml;
+
+public class InvertBool : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) =>
+        !(bool)value;
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+        !(bool)value;
+}
