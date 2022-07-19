374ad46530c0ed68898caae9554aa177a54de0bc
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-07-19 18:09:02 +0300

    Started working on Settings page

--- IcyLauncher.Core/Services/Updater.cs
+++ IcyLauncher.Core/Services/Updater.cs
@@ -5,5 +5,9 @@ namespace IcyLauncher.Core.Services;
 public class Updater
 {
     public readonly Version CurrentAppVersion = Assembly.GetExecutingAssembly().GetName().Version is Version ver ? ver.TrimZeros() : new(0, 1);
-    public readonly Version CurrentApiVersion = new Version(0,1).TrimZeros() is Version ver ? ver : new(0, 1);
+    public readonly Version CurrentApiVersion = new Version(0, 0, 0, 1) is Version ver ? ver : new(0, 1);
+
+    public readonly DateTime LastChecked = DateTime.Now;
+
+    public readonly bool UpdateAvailable = false;
 }
--- IcyLauncher/ViewModels/SettingsViewModel.cs
+++ IcyLauncher/ViewModels/SettingsViewModel.cs
@@ -1,26 +1,68 @@
-﻿namespace IcyLauncher.ViewModels;
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
+using Windows.UI;
+
+namespace IcyLauncher.ViewModels;
 
 public partial class SettingsViewModel : ObservableObject
 {
-    public Configuration Configuration;
     readonly ILogger<ProfilesViewModel> logger;
     readonly ConfigurationManager configurationManager;
     readonly ThemeManager themeManager;
     readonly IFileSystem fileSystem;
 
-    public SettingsViewModel(IOptions<Configuration> configuration, ILogger<ProfilesViewModel> logger, ConfigurationManager configurationManager, ThemeManager themeManager, IFileSystem fileSystem)
+    public readonly Configuration Configuration;
+    public readonly Updater Updater;
+
+    public SettingsViewModel(IOptions<Configuration> configuration, ILogger<ProfilesViewModel> logger, ConfigurationManager configurationManager, ThemeManager themeManager, IFileSystem fileSystem, Updater updater)
     {
-        Configuration = configuration.Value;
         this.logger = logger;
         this.configurationManager = configurationManager;
         this.themeManager = themeManager;
         this.fileSystem = fileSystem;
+
+        Configuration = configuration.Value;
+        Updater = updater;
     }
 
+
+    public void OnPageLoaded(object _, RoutedEventArgs _1) =>
+        SetUIUpdate(Updater.UpdateAvailable);
+
+
+    [ObservableProperty]
+    string updateStatus = "Last checked: 0 min";
+
+    [ObservableProperty]
+    Visibility updateBadgeVisibility = Visibility.Collapsed;
+
+    public SolidColorBrush UpdateButtonBackground = new(Colors.Transparent);
+    public SolidColorBrush UpdateButtonBorderBrush = new(Colors.Transparent);
+
+    public void SetUIUpdate(bool isEnabled)
+    {
+        if (isEnabled)
+        {
+            UpdateStatus = "New update available!";
+            UpdateBadgeVisibility = Visibility.Visible;
+            UpdateButtonBackground.Color = Color.FromArgb(20, 255, 255, 0);
+            UpdateButtonBorderBrush.Color = Color.FromArgb(50, 255, 255, 0);
+            return;
+        }
+
+        var dif = DateTime.Now - Updater.LastChecked;
+        UpdateStatus = dif.Hours == 0 ? $"Last checked: {Math.Round(dif.TotalMinutes, 0)} min" : $"Last checked: {Math.Round(dif.TotalHours, 1)} hrs";
+        UpdateBadgeVisibility = Visibility.Collapsed;
+        UpdateButtonBackground.Color = Colors.Transparent;
+        UpdateButtonBorderBrush.Color = Colors.Transparent;
+    }
 
     [ICommand]
-    void Test()
+    void Update()
     {
-
+        SetUIUpdate(Enabl);
+        Enabl = !Enabl;
     }
+
+    bool Enabl = false;
 }
