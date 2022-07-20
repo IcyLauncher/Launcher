9ee87cdda6d72d9d3306e34dfbebc0bd3f512080
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-07-20 15:51:57 +0300

    Settings layout

--- IcyLauncher/ViewModels/SettingsViewModel.cs
+++ IcyLauncher/ViewModels/SettingsViewModel.cs
@@ -9,17 +9,19 @@ public partial class SettingsViewModel : ObservableObject
     readonly ILogger<ProfilesViewModel> logger;
     readonly ConfigurationManager configurationManager;
     readonly ThemeManager themeManager;
+    readonly WindowHandler windowHandler;
     readonly IFileSystem fileSystem;
     readonly INavigation navigation;
 
     public readonly Configuration Configuration;
     public readonly Updater Updater;
 
-    public SettingsViewModel(IOptions<Configuration> configuration, ILogger<ProfilesViewModel> logger, ConfigurationManager configurationManager, ThemeManager themeManager, IFileSystem fileSystem, Updater updater, INavigation navigation)
+    public SettingsViewModel(IOptions<Configuration> configuration, ILogger<ProfilesViewModel> logger, ConfigurationManager configurationManager, ThemeManager themeManager, WindowHandler windowHandler, IFileSystem fileSystem, Updater updater, INavigation navigation)
     {
         this.logger = logger;
         this.configurationManager = configurationManager;
         this.themeManager = themeManager;
+        this.windowHandler = windowHandler;
         this.fileSystem = fileSystem;
         this.navigation = navigation;
 
@@ -29,12 +31,13 @@ public partial class SettingsViewModel : ObservableObject
 
 
     public void OnPageLoaded(object _, RoutedEventArgs _1) =>
-        SetUIUpdate(Updater.UpdateAvailable);
+        IsUpdateVisible = Updater.IsUpdateAvailable;
 
 
     [ICommand]
     void Debug()
     {
+        themeManager.LoadTheme(Theme.Dark);
     }
 
 
@@ -47,30 +50,41 @@ public partial class SettingsViewModel : ObservableObject
     public SolidColorBrush UpdateButtonBackground = new(Colors.Transparent);
     public SolidColorBrush UpdateButtonBorderBrush = new(Colors.Transparent);
 
-    public void SetUIUpdate(bool isEnabled)
+    bool isUpdateVisible = false;
+    public bool IsUpdateVisible
     {
-        if (isEnabled)
+        get => isUpdateVisible;
+        set
         {
-            UpdateStatus = "New update available!";
-            UpdateBadgeVisibility = Visibility.Visible;
-            UpdateButtonBackground.Color = Color.FromArgb(20, 255, 255, 0);
-            UpdateButtonBorderBrush.Color = Color.FromArgb(50, 255, 255, 0);
-            return;
-        }
+            if (isUpdateVisible == value)
+                return;
 
-        var dif = DateTime.Now - Updater.LastChecked;
-        UpdateStatus = dif.Hours == 0 ? $"Last checked: {Math.Round(dif.TotalMinutes, 0)} min" : $"Last checked: {Math.Round(dif.TotalHours, 1)} hrs";
-        UpdateBadgeVisibility = Visibility.Collapsed;
-        UpdateButtonBackground.Color = Colors.Transparent;
-        UpdateButtonBorderBrush.Color = Colors.Transparent;
+            isUpdateVisible = value;
+            logger.Log("Updating update visibility");
+
+            if (value)
+            {
+                UpdateStatus = "New update available!";
+                UpdateBadgeVisibility = Visibility.Visible;
+                UpdateButtonBackground.Color = Color.FromArgb(20, 255, 255, 0);
+                UpdateButtonBorderBrush.Color = Color.FromArgb(50, 255, 255, 0);
+                return;
+            }
+
+            var dif = DateTime.Now - Updater.LastChecked;
+            UpdateStatus = dif.Hours == 0 ? $"Last checked: {Math.Round(dif.TotalMinutes, 0)} min" : $"Last checked: {Math.Round(dif.TotalHours, 1)} hrs";
+            UpdateBadgeVisibility = Visibility.Collapsed;
+            UpdateButtonBackground.Color = Colors.Transparent;
+            UpdateButtonBorderBrush.Color = Colors.Transparent;
+        }
     }
 
     [ICommand]
     void Update()
     {
-        SetUIUpdate(Enabl);
-        Enabl = !Enabl;
+        IsUpdateVisible = !IsUpdateVisible;
     }
 
-    bool Enabl = false;
+
+
 }
