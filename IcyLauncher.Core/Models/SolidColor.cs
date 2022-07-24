78b0f728b43b19251846d97825b4b0c2e2044a31
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-07-24 18:41:32 +0300

    Added SolidColor + DataTemplate

---
+++ IcyLauncher.Core/Models/SolidColor.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI;
+
+namespace IcyLauncher.Core.Models;
+
+public class SolidColor
+{
+    public SolidColor()
+    {
+    }
+
+    public SolidColor(Color color, string name)
+    {
+        Color = color;
+        Name = name;
+    }
+
+    public Color Color { get; set; } = default!;
+    public string Name { get; set; } = default!;
+}
---
+++ IcyLauncher/UI/Xaml/SolidColorTemplate.xaml.cs
@@ -0,0 +1,44 @@
+﻿using IcyLauncher.ViewModels;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Shapes;
+
+namespace IcyLauncher.UI.Xaml;
+
+public partial class SolidColorTemplate : ResourceDictionary
+{
+    public SolidColorTemplate() =>
+        InitializeComponent();
+
+
+    public readonly BannerSettingsViewModel viewModel = App.Provider.GetRequiredService<BannerSettingsViewModel>();
+
+
+    private void OnRootLayoutPointerPressed(object sender, PointerRoutedEventArgs e)
+    {
+        if (!e.GetCurrentPoint((UIElement)sender).Properties.IsRightButtonPressed)
+            return;
+
+        var senderElement = (FrameworkElement)sender;
+        FlyoutBase.GetAttachedFlyout(senderElement).ShowAt(senderElement);
+    }
+
+    private void OnRootLayoutLoaded(object sender, RoutedEventArgs e)
+    {
+        var rootLayout = (Grid)sender;
+        var rect = (Rectangle)rootLayout.Children[0];
+        var name = (TextBlock)rootLayout.Children[1];
+
+        var ele = viewModel.SolidColors.Where(solCol => solCol.Color == ((SolidColorBrush)rect.Fill).Color && solCol.Name == name.Text).ToArray();
+        if (ele.Length == 0)
+            return;
+
+        var senderElement = (FrameworkElement)sender;
+        var flyout = (MenuFlyout)FlyoutBase.GetAttachedFlyout(senderElement);
+
+        ((MenuFlyoutItem)flyout.Items[0]).Click += async (s, e) =>
+            await viewModel.DeleteSolidColor(ele[0]);
+    }
+}
