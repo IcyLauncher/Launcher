cad5dba36abafd9f54e9f316e34e40b54cc2a37a
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-07-24 18:40:50 +0300

    Added MiniLauncher & Styled GridViewItem & Styled Flyout & Updated Combobox background

---
+++ IcyLauncher/UI/MiniLauncher.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+
+namespace IcyLauncher.UI;
+
+public class MiniLauncher : ContentControl
+{
+    public Brush BannerBrush
+    {
+        get => (Brush)GetValue(BannerBrushProperty);
+        set => SetValue(BannerBrushProperty, value);
+    }
+
+    public static readonly DependencyProperty BannerBrushProperty = DependencyProperty.Register(
+        "BannerBrush",
+        typeof(Brush),
+        typeof(MiniLauncher),
+        new PropertyMetadata(new SolidColorBrush(Colors.White)));
+}
