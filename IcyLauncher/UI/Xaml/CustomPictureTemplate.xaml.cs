0ac71879be850ae03f878b7f15a3f856a0a2bacd
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-08-01 19:06:12 +0300

    Added CustomPictureTemplate

---
+++ IcyLauncher/UI/Xaml/CustomPictureTemplate.xaml.cs
@@ -0,0 +1,31 @@
+﻿using IcyLauncher.ViewModels;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Input;
+
+namespace IcyLauncher.UI.Xaml;
+
+public partial class CustomPictureTemplate : ResourceDictionary
+{
+    public CustomPictureTemplate() =>
+        InitializeComponent();
+
+
+    BannerSettingsViewModel viewModel = default!;
+
+
+    void OnRootLayoutPointerPressed(object sender, PointerRoutedEventArgs e)
+    {
+        if (!e.GetCurrentPoint((UIElement)sender).Properties.IsRightButtonPressed)
+            return;
+
+        if (viewModel is null)
+            viewModel = App.Provider.GetRequiredService<BannerSettingsViewModel>();
+
+        var senderElement = (FrameworkElement)sender;
+        FlyoutBase.GetAttachedFlyout(senderElement).ShowAt(senderElement);
+    }
+
+    async void OnDeleteClick(object sender, RoutedEventArgs e) =>
+        await viewModel.RemoveCustomPicture((string)((MenuFlyoutItem)sender).Tag);
+}
