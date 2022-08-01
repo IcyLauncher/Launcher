d978a1da993fad8eda71c8152f215dea8768738e
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-08-01 19:06:00 +0300

    Optimized SolidColorTemplate

--- IcyLauncher/UI/Xaml/SolidColorTemplate.xaml.cs
+++ IcyLauncher/UI/Xaml/SolidColorTemplate.xaml.cs
@@ -1,9 +1,6 @@
-﻿using IcyLauncher.ViewModels;
-using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Shapes;
 
 namespace IcyLauncher.UI.Xaml;
 
@@ -25,20 +22,6 @@ public partial class SolidColorTemplate : ResourceDictionary
         FlyoutBase.GetAttachedFlyout(senderElement).ShowAt(senderElement);
     }
 
-    private void OnRootLayoutLoaded(object sender, RoutedEventArgs e)
-    {
-        var rootLayout = (Grid)sender;
-        var rect = (Rectangle)rootLayout.Children[0];
-        var name = (TextBlock)rootLayout.Children[1];
-
-        var ele = SolidColors.Container.Where(solCol => solCol.Color == ((SolidColorBrush)rect.Fill).Color && solCol.Name == name.Text).ToArray();
-        if (ele.Length == 0)
-            return;
-
-        var senderElement = (FrameworkElement)sender;
-        var flyout = (MenuFlyout)FlyoutBase.GetAttachedFlyout(senderElement);
-
-        ((MenuFlyoutItem)flyout.Items[0]).Click += (s, e) =>
-            SolidColors.Container.Remove(ele[0]);
-    }
+    private void OnDeleteClick(object sender, RoutedEventArgs e) =>
+            SolidColors.Container.Remove((SolidColor)((MenuFlyoutItem)sender).Tag);
 }
