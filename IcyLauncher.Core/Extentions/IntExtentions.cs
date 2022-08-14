dc2d914f5d16aa804023f54649c62828706af2f6
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-08-14 15:40:30 +0300

    New round down int extention

---
+++ IcyLauncher.Core/Extentions/IntExtentions.cs
@@ -0,0 +1,7 @@
+﻿namespace IcyLauncher.Core.Extentions;
+
+public static class IntExtentions
+{
+    public static int RoundDown(this int input, int[] list) =>
+        list.Where(x => x <= input) is IEnumerable<int> res && res.Count() > 0 ? res.Last() : 0;
+}
