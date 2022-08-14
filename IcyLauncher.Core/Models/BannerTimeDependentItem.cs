ce9d4fce1e286866ac0125017c778b00d145c862
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-08-14 15:40:16 +0300

    Added TimeDependent banner data template

---
+++ IcyLauncher.Core/Models/BannerTimeDependentItem.cs
@@ -0,0 +1,38 @@
+﻿namespace IcyLauncher.Core.Models;
+
+public class BannerTimeDependentItem
+{
+    public BannerTimeDependentItem() { }
+
+    public BannerTimeDependentItem(string title,
+        string i_0,
+        string i_3,
+        string i_6,
+        string i_9,
+        string i_12,
+        string i_15,
+        string i_18,
+        string i_21)
+    {
+        Title = title;
+        I_0 = i_0;
+        I_3 = i_3;
+        I_6 = i_6;
+        I_9 = i_9;
+        I_12 = i_12;
+        I_15 = i_15;
+        I_18 = i_18;
+        I_21 = i_21;
+    }
+
+    public string Title { get; set; } = "N/A";
+    public string I_0 { get; set; } = "ms-appx:///Assets/Banners/NoBanner.png";
+    public string I_3 { get; set; } = "ms-appx:///Assets/Banners/NoBanner.png";
+    public string I_6 { get; set; } = "ms-appx:///Assets/Banners/NoBanner.png";
+    public string I_9 { get; set; } = "ms-appx:///Assets/Banners/NoBanner.png";
+    public string I_12 { get; set; } = "ms-appx:///Assets/Banners/NoBanner.png";
+    public string I_15 { get; set; } = "ms-appx:///Assets/Banners/NoBanner.png";
+    public string I_18 { get; set; } = "ms-appx:///Assets/Banners/NoBanner.png";
+    public string I_21 { get; set; } = "ms-appx:///Assets/Banners/NoBanner.png";
+
+}
---
+++ IcyLauncher/UI/Xaml/BannerTimeDependentTemplate.xaml.cs
@@ -0,0 +1,57 @@
+﻿using IcyLauncher.ViewModels;
+using Microsoft.UI;
+using Microsoft.UI.Composition;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Shapes;
+
+namespace IcyLauncher.UI.Xaml;
+
+public partial class BannerTimeDependentTemplate : ResourceDictionary
+{
+    public BannerTimeDependentTemplate() =>
+        InitializeComponent();
+
+
+    ImagingUtility imagingUtility = default!;
+
+
+    private void OnImageLoaded(object sender, RoutedEventArgs e)
+    {
+        if (imagingUtility is null)
+            imagingUtility = App.Provider.GetRequiredService<ImagingUtility>();
+
+        var grid = (Rectangle)sender;
+        var timeDependent = (BannerTimeDependentItem)grid.DataContext;
+
+
+        imagingUtility.InitializeUIElement(grid, out var bannerCompositor, out var banner);
+
+        if (bannerCompositor is null || banner is null)
+            return;
+
+        AddImagePart(banner, bannerCompositor, timeDependent.I_3, 1);
+        AddImagePart(banner, bannerCompositor, timeDependent.I_6, 2);
+        AddImagePart(banner, bannerCompositor, timeDependent.I_9, 3);
+        AddImagePart(banner, bannerCompositor, timeDependent.I_12, 4);
+        AddImagePart(banner, bannerCompositor, timeDependent.I_15, 5);
+        AddImagePart(banner, bannerCompositor, timeDependent.I_21, 7);
+
+        var backgroundBrush = imagingUtility.CreateImageBrush(bannerCompositor, new(timeDependent.I_0), CompositionStretch.UniformToFill);
+        var backgroundVisual = imagingUtility.CreateSpriteVisual(bannerCompositor, new(270f, 66f), backgroundBrush);
+
+        banner.Children.InsertAtBottom(backgroundVisual);
+    }
+
+    void AddImagePart(ContainerVisual container, Compositor compositor, string image, int index) =>
+        container.Children.InsertAtTop(imagingUtility.CreateSpriteVisual(compositor,
+            new(286.875f - (33.75f * index), 66f),
+            imagingUtility.CreateMaskBrush(compositor,
+                imagingUtility.CreateImageBrush(compositor, new(image), CompositionStretch.UniformToFill, 1f),
+                imagingUtility.CreateGradientBrush(
+                    compositor,
+                    new(0f, 0f), new(1f, 0f),
+                    new[] { (0f, Colors.Transparent), (10f * index / 100, Colors.White) })),
+            new((33.75f * index) - 16.875f, 0f, 0f)));
+}
