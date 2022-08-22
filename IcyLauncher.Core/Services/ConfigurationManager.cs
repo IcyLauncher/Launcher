79b8ed5866179e59dedf0e626758e205241b81e9
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-08-22 18:58:13 +0300

    Fixed loading external configurations

--- IcyLauncher.Core/Services/ConfigurationManager.cs
+++ IcyLauncher.Core/Services/ConfigurationManager.cs
@@ -22,14 +22,33 @@ public class ConfigurationManager
     public string Export() =>
         converter.ToString(configuration);
 
-    public void Load(Configuration input, bool IgnoreTheme = false)
+    public void Load(Configuration input, bool ignoreTheme = false)
     {
-        configuration.Launcher = input.Launcher;
-        if (!IgnoreTheme)
-            configuration.Apperance = input.Apperance;
-        configuration.Weather = input.Weather;
-        configuration.DateTime = input.DateTime;
-        configuration.Developer = input.Developer;
+        if (!ignoreTheme)
+            ThemeManager.CopyTheme(configuration.Apperance.Colors, input.Apperance.Colors);
+
+        configuration.Launcher.IsDiscordEnabled = input.Launcher.IsDiscordEnabled;
+        configuration.Launcher.IsScrollbarsEnabled = input.Launcher.IsScrollbarsEnabled;
+        configuration.Launcher.AtMinecraftaunch = input.Launcher.AtMinecraftaunch;
+        configuration.Launcher.TexturepackDirectory = input.Launcher.TexturepackDirectory;
+        configuration.Launcher.VersionsDirectory = input.Launcher.VersionsDirectory;
+
+        configuration.Apperance.HomeBanner = input.Apperance.HomeBanner;
+        configuration.Apperance.SelectedHomeBanner = input.Apperance.SelectedHomeBanner;
+        configuration.Apperance.HomeBannerUri = input.Apperance.HomeBannerUri;
+        configuration.Apperance.Blur = input.Apperance.Blur;
+        configuration.Apperance.UseDarkModeBlur = input.Apperance.UseDarkModeBlur;
+
+        configuration.Weather.IsEnabled = input.Weather.IsEnabled;
+        configuration.Weather.IsAutoLocationEnabled = input.Weather.IsAutoLocationEnabled;
+        configuration.Weather.Location = input.Weather.Location;
+        configuration.Weather.Unit = input.Weather.Unit;
+
+        configuration.DateTime.DateFormat = input.DateTime.DateFormat;
+        configuration.DateTime.TimeFormat = input.DateTime.TimeFormat;
+
+        configuration.Developer.IsOneClickEnabled = input.Developer.IsOneClickEnabled;
+        configuration.Developer.IsWarningEnabled = input.Developer.IsWarningEnabled;
 
         logger.Log($"Loaded app configuration from string");
     }
