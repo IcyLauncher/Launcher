88167422649678a4a31bbaad8bce40026f4ff885
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-09-10 18:07:19 +0300

    Removed unused Win32 methods

--- IcyLauncher.Helpers/Win32.cs
+++ IcyLauncher.Helpers/Win32.cs
@@ -1,6 +1,5 @@
 ﻿#pragma warning disable CA1401
 #pragma warning disable CA2211
-#pragma warning disable IDE0060
 
 using System.Runtime.InteropServices;
 
@@ -13,7 +12,6 @@ public class Win32
     public delegate int SUBCLASSPROC(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam, IntPtr uIdSubclass, uint dwRefData);
 
     public static WinProc? NewWndProc { get; set; }
-    public static SUBCLASSPROC? SubClassDelegate { get; set; }
     public static IntPtr OldWndProc { get; set; } = IntPtr.Zero;
 
     public static int MinWidth = 0;
@@ -45,61 +43,6 @@ public class Win32
         public int apartmentType;
     }
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct RECT
-    {
-        public RECT(int left, int top, int right, int bottom)
-        {
-            this.left = left;
-            this.top = top;
-            this.right = right;
-            this.bottom = bottom;
-        }
-
-        public int left;
-        public int top;
-        public int right;
-        public int bottom;
-    }
-
-    public enum DWMWINDOWATTRIBUTE
-    {
-        DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
-        DWMWA_MICA_EFFECT = 1029
-    };
-
-    public enum AccentState
-    {
-        ACCENT_DISABLED = 0,
-        ACCENT_ENABLE_GRADIENT = 1,
-        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
-        ACCENT_ENABLE_BLURBEHIND = 3,
-        ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
-        ACCENT_INVALID_STATE = 5
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct AccentPolicy
-    {
-        public AccentState AccentState;
-        public uint AccentFlags;
-        public uint GradientColor;
-        public uint AnimationId;
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct WindowCompositionAttributeData
-    {
-        public WindowCompositionAttribute Attribute;
-        public IntPtr Data;
-        public int SizeOfData;
-    }
-
-    public enum WindowCompositionAttribute
-    {
-        WCA_ACCENT_POLICY = 19
-    }
-
 
     [DllImport("user32.dll")]
     public static extern IntPtr GetDpiForWindow(IntPtr hwnd);
@@ -112,45 +55,6 @@ public class Win32
 
     [DllImport("CoreMessaging.dll")]
     public static extern IntPtr CreateDispatcherQueueController([In] DISPATCHERQUEUEOPTIONS options, [In, Out, MarshalAs(UnmanagedType.IUnknown)] ref object? dispatcherQueueController);
-
-    [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
-
-    [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    public static extern bool FillRect(IntPtr hdc, [In] ref RECT rect, IntPtr hbrush);
-
-    [DllImport("Gdi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    public static extern IntPtr CreateSolidBrush(int crColor);
-
-    [DllImport("Gdi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    public static extern bool DeleteObject([In] IntPtr hObject);
-
-    [DllImport("Comctl32.dll", SetLastError = true)]
-    public static extern bool SetWindowSubclass(IntPtr hWnd, SUBCLASSPROC pfnSubclass, uint uIdSubclass, uint dwRefData);
-
-    [DllImport("Comctl32.dll", SetLastError = true)]
-    public static extern int DefSubclassProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
-
-    [DllImport("User32.dll", CharSet = CharSet.Auto, EntryPoint = "SetWindowLong")]
-    public static extern bool SetWindowLongPtr32(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
-
-    [DllImport("User32.dll", CharSet = CharSet.Auto, EntryPoint = "SetWindowLongPtr")]
-    public static extern bool SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
-
-    [DllImport("User32.dll", EntryPoint = "GetWindowLong", CharSet = CharSet.Auto)]
-    public static extern long GetWindowLong32(IntPtr hWnd, int nIndex);
-
-    [DllImport("User32.dll", EntryPoint = "GetWindowLongPtr", CharSet = CharSet.Auto)]
-    public static extern long GetWindowLongPtr64(IntPtr hWnd, int nIndex);
-
-    [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-    public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
-
-    [DllImport("dwmapi.dll", PreserveSig = true)]
-    public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref bool attrValue, int attrSize);
-
-    [DllImport("user32.dll")]
-    public static extern bool SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
 
     [DllImport("winbrand.dll", CharSet = CharSet.Unicode)]
     public static extern string BrandingFormatString(string format);
@@ -168,30 +72,4 @@ public class Win32
         }
         return CallWindowProc(OldWndProc, hWnd, msg, wParam, lParam);
     }
-
-    public static int WindowSubClass(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam, IntPtr uIdSubclass, uint dwRefData)
-    {
-        switch (uMsg)
-        {
-            case 0x0014:
-                {
-                    GetClientRect(hWnd, out RECT rect);
-                    rect.right = 0;
-
-                    IntPtr hBrush = CreateSolidBrush(System.Drawing.ColorTranslator.ToWin32(System.Drawing.Color.Magenta));
-
-                    FillRect(wParam, ref rect, hBrush);
-                    DeleteObject(hBrush);
-                    return 1;
-                }
-        }
-
-        return DefSubclassProc(hWnd, uMsg, wParam, lParam);
-    }
-
-    public static bool SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong) =>
-        IntPtr.Size == 4 ? SetWindowLongPtr32(hWnd, nIndex, dwNewLong) : SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
-
-    public static long GetWindowLong(IntPtr hWnd, int nIndex) =>
-        IntPtr.Size == 4 ? GetWindowLong32(hWnd, nIndex) : GetWindowLongPtr64(hWnd, nIndex);
 }
