185678f68a1a65e2a4fa3228d1818c23b797a3dd
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-09-24 18:38:15 +0300

    Fixed wrong name on SettingsViewModel folder

--- IcyLauncher.WinUI/ViewModels/SettingsViewModels/DeveloperSettingsViewModel.cs
+++ IcyLauncher.WinUI/ViewModels/SettingsViewModels/DeveloperSettingsViewModel.cs
@@ -2,23 +2,32 @@
 
 public partial class DeveloperSettingsViewModel : ObservableObject
 {
-    readonly INavigation navigation;
     readonly ILogger<ProfilesViewModel> logger;
+    readonly ConfigurationManager configurationManager;
     readonly ThemeManager themeManager;
     readonly WindowHandler windowHandler;
+    readonly IConverter converter;
+    readonly IMessage message;
+    readonly INavigation navigation;
 
     public readonly Configuration Configuration;
 
     public DeveloperSettingsViewModel(
         ILogger<ProfilesViewModel> logger,
         IOptions<Configuration> configuration,
+        ConfigurationManager configurationManager,
         ThemeManager themeManager,
         WindowHandler windowHandler,
+        IConverter converter,
+        IMessage message,
         INavigation navigation)
     {
         this.logger = logger;
+        this.configurationManager = configurationManager;
         this.themeManager = themeManager;
         this.windowHandler = windowHandler;
+        this.converter = converter;
+        this.message = message;
         this.navigation = navigation;
 
         Configuration = configuration.Value;
