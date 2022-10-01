ba8ee772c892918039a586f8da73ac323b8e56d1
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-10-01 14:20:16 +0300

    Started working on FileSystem debug tab

---
+++ IcyLauncher.WinUI/ViewModels/SettingsViewModels/DeveloperViewModels/IConverterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI;
+using Newtonsoft.Json;
+
+namespace IcyLauncher.WinUI.ViewModels;
+
+public partial class DeveloperSettingsViewModel : ObservableObject
+{
+    [ObservableProperty]
+    SolidColor iConverter_object = new(Colors.Red, "Red");
+
+    [ObservableProperty]
+    string iConverter_input = "";
+
+
+    [ObservableProperty]
+    Formatting iConverter_formatting = Formatting.None;
+
+    [RelayCommand]
+    void IConverter_ToString() =>
+        IConverter_input = converter.ToString(IConverter_object, IConverter_formatting);
+
+    [RelayCommand]
+    async void IConverter_ToObject()
+    {
+        if (converter.TryToObject(out SolidColor? solidColor, IConverter_input) == true)
+            IConverter_object = solidColor!;
+        else
+            await message.ShowAsync("Something went wrong :(", "It looks like this is not a valid SolidColor.\nPlease verify the input is a proper JSON and every property is being set.", closeButton: "Ok");
+    }
+
+    [RelayCommand]
+    async void IConverter_TryToObject() =>
+            await message.ShowAsync("TryToObject - Result", $"Method testing returned: {converter.TryToObject(out SolidColor? _, IConverter_input)}", closeButton: "Ok");
+}
---
+++ IcyLauncher.WinUI/ViewModels/SettingsViewModels/DeveloperViewModels/IFileSystemViewModel.cs
@@ -0,0 +1,52 @@
+﻿namespace IcyLauncher.WinUI.ViewModels;
+
+public partial class DeveloperSettingsViewModel : ObservableObject
+{
+    [ObservableProperty]
+    string iFileSystem_path = "";
+
+    [ObservableProperty]
+    string iFileSystem_destination = "";
+
+    [ObservableProperty]
+    bool iFileSystem_overwrite = false;
+
+
+    [RelayCommand]
+    async Task IFileSystem_FileExists() =>
+            await message.ShowAsync("FileExsists - Result", $"Method testing returned: {fileSystem.FileExists(IFileSystem_path)}", closeButton: "Ok");
+
+
+    [RelayCommand]
+    async Task IFileSystem_FileWritable() =>
+            await message.ShowAsync("FileWritable - Result", $"Method testing returned: {fileSystem.FileWritable(IFileSystem_path)}", closeButton: "Ok");
+
+
+    [RelayCommand]
+    async Task IFileSystem_CopyFile()
+    {
+        try
+        {
+            fileSystem.CopyFile(IFileSystem_path, IFileSystem_destination, IFileSystem_overwrite);
+            await message.ShowAsync("CopyFile - Result", "Method completed", closeButton: "Ok");
+        }
+        catch (Exception ex)
+        {
+            await message.ShowAsync("CopyFile - Result", $"Method threw exception: {ex.Format()}", closeButton: "Ok");
+        }
+    }
+
+    [RelayCommand(AllowConcurrentExecutions = false, IncludeCancelCommand = true)]
+    async Task IFileSystem_CopyFileAAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            await fileSystem.CopyFileAsync(IFileSystem_path, IFileSystem_destination, IFileSystem_overwrite, cancellationToken);
+            await message.ShowAsync("CopyFile - Result", "Method completed", closeButton: "Ok");
+        }
+        catch (Exception ex)
+        {
+            await message.ShowAsync("CopyFile - Result", $"Method threw exception: {ex.Format()}", closeButton: "Ok");
+        }
+    }
+}
