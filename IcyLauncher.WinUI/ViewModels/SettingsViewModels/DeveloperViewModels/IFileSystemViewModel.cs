7e59c080e933b6047adee84f2d0c09f2175455ac
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-10-02 10:41:54 +0300

    Finished IFileSystem debug tab

--- IcyLauncher.WinUI/ViewModels/SettingsViewModels/DeveloperViewModels/IFileSystemViewModel.cs
+++ IcyLauncher.WinUI/ViewModels/SettingsViewModels/DeveloperViewModels/IFileSystemViewModel.cs
@@ -14,6 +14,9 @@ public partial class DeveloperSettingsViewModel : ObservableObject
     [ObservableProperty]
     int iFileSystem_timeout = 60000;
 
+    [ObservableProperty]
+    string iFileSystem_content = "Hello World! :)";
+
 
     [RelayCommand]
     async Task IFileSystem_FileExists()
@@ -61,15 +64,8 @@ public partial class DeveloperSettingsViewModel : ObservableObject
     [RelayCommand(AllowConcurrentExecutions = false, IncludeCancelCommand = true)]
     async Task IFileSystem_CopyFileAAsync(CancellationToken cancellationToken)
     {
-        try
-        {
             await fileSystem.CopyFileAsync(IFileSystem_path, IFileSystem_destination, IFileSystem_overwrite, cancellationToken);
             await message.ShowAsync("fileSystem.CopyFileAsync()", "Method completed", closeButton: "Ok");
-        }
-        catch (Exception ex)
-        {
-            await message.ShowAsync("fileSystem.CopyFileAsync()", $"Method completed.\nException{ex.Format()}", closeButton: "Ok");
-        }
     }
 
 
@@ -98,6 +94,94 @@ public partial class DeveloperSettingsViewModel : ObservableObject
         catch (Exception ex)
         {
             await message.ShowAsync("fileSystem.DeleteFileAsync()", $"Method completed.\nException{ex.Format()}", closeButton: "Ok");
+        }
+    }
+
+
+    [RelayCommand(AllowConcurrentExecutions = false, IncludeCancelCommand = true)]
+    async Task IFileSystem_WaitForFileLockAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            bool result = await fileSystem.WaitForFileLockAsync(IFileSystem_path, IFileSystem_timeout, cancellationToken);
+            await message.ShowAsync("fileSystem.WaitForFileLockAsync()", $"Method completed.\nResult: {result}", closeButton: "Ok");
+        }
+        catch (Exception ex)
+        {
+            await message.ShowAsync("fileSystem.WaitForFileLockAsync()", $"Method completed.\nException{ex.Format()}", closeButton: "Ok");
+        }
+    }
+
+
+    [RelayCommand(AllowConcurrentExecutions = false, IncludeCancelCommand = true)]
+    async Task IFileSystem_ReadAsTextAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            string result = await fileSystem.ReadAsTextAsync(IFileSystem_path, cancellationToken);
+            await message.ShowAsync("fileSystem.ReadAsTextAsync()", $"Method completed.\nResult: {(result.Length > 200 ? $"{result[..200]}..." : result)}", closeButton: "Ok");
+        }
+        catch (Exception ex)
+        {
+            await message.ShowAsync("fileSystem.ReadAsTextAsync()", $"Method completed.\nException{ex.Format()}", closeButton: "Ok");
+        }
+    }
+
+    [RelayCommand(AllowConcurrentExecutions = false, IncludeCancelCommand = true)]
+    async Task IFileSystem_SaveAsTextAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            await fileSystem.SaveAsTextAsync(IFileSystem_path, IFileSystem_content, IFileSystem_overwrite, cancellationToken);
+            await message.ShowAsync("fileSystem.SaveAsTextAsync()", "Method completed.", closeButton: "Ok");
+        }
+        catch (Exception ex)
+        {
+            await message.ShowAsync("fileSystem.SaveAsTextAsync()", $"Method completed.\nException{ex.Format()}", closeButton: "Ok");
+        }
+    }
+
+
+    [RelayCommand]
+    async Task IFileSystem_DirectoryExists()
+    {
+        try
+        {
+            bool result = fileSystem.DirectoryExists(IFileSystem_path);
+            await message.ShowAsync("fileSystem.DirectoryExists()", $"Method completed.\nResult: {result}", closeButton: "Ok");
+        }
+        catch (Exception ex)
+        {
+            await message.ShowAsync("fileSystem.DirectoryExists()", $"Method completed.\nException{ex.Format()}", closeButton: "Ok");
+        }
+    }
+
+    [RelayCommand]
+    async Task IFileSystem_DirectoryWritable()
+    {
+        try
+        {
+            bool result = fileSystem.DirectoryWritable(IFileSystem_path);
+            await message.ShowAsync("fileSystem.DirectoryWritable()", $"Method completed.\nResult: {result}", closeButton: "Ok");
+        }
+        catch (Exception ex)
+        {
+            await message.ShowAsync("fileSystem.DirectoryWritable()", $"Method completed.\nException{ex.Format()}", closeButton: "Ok");
+        }
+    }
+
+
+    [RelayCommand]
+    async Task IFileSystem_CreateDirectory()
+    {
+        try
+        {
+            fileSystem.CreateDirectory(IFileSystem_path);
+            await message.ShowAsync("fileSystem.CreateDirectory()", "Method completed.", closeButton: "Ok");
+        }
+        catch (Exception ex)
+        {
+            await message.ShowAsync("fileSystem.CreateDirectory()", $"Method completed.\nException{ex.Format()}", closeButton: "Ok");
         }
     }
 }
