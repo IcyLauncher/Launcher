a9700964ff563ba644d6589c67f40b452839c28d
IcySnex <81762750+IcySnex@users.noreply.github.com>
2022-10-03 19:23:25 +0300

    Added ability to add BitmapCreateOptions to AsImage() extention

--- IcyLauncher.Helpers/Extentions/String.cs
+++ IcyLauncher.Helpers/Extentions/String.cs
@@ -9,8 +9,8 @@ public static class StringExtentions
     public static Uri FromAssets(this string input) =>
         new($"ms-appx:///Assets/{input}");
 
-    public static BitmapImage AsImage(this string input, bool fromAssets = true) =>
-        new(fromAssets ? input.FromAssets() : new(input));
+    public static BitmapImage AsImage(this string input, bool fromAssets = true, BitmapCreateOptions createOptions = BitmapCreateOptions.None) =>
+        new(fromAssets ? input.FromAssets() : new(input)) {  CreateOptions = createOptions };
 
     public static Type? AsType(this string input, string assembly = "IcyLauncher.WinUI") =>
         Type.GetType($"{assembly}.{input}, {assembly}");
